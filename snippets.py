3bfd9df563340a8893b0cd345ba4f0012dcf054f
Jon Thorpe <jonthrp@yahoo.com>
2016-12-22 16:56:40 -0500

    Added error handling

--- snippets.py
+++ snippets.py
@@ -14,23 +14,31 @@ def put(name, snippet):
     """Store a snippet with an associated name."""
     logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
     cursor = connection.cursor()
-    command = "insert into snippets values (%s, %s)"
-    cursor.execute(command, (name, snippet))
-    connection.commit()
+    try:
+        command = "insert into snippets values (%s, %s)"
+        cursor.execute(command, (name, snippet))
+    except psycopg2.IntegrityError as e:
+        connection.rollback()
+        command = "update snippets set message=%s where keyword=%s"
+        cursor.execute(command, (snippet, name))
     logging.debug("Snippet stored successfully.")
     return name, snippet
 
 
 def get(name):
-    """Retrieve the snippet with a given name."""
+    """Retrieve the snippet with a given name. If no snippet exists with that name,
+    return '404: Snippet Not Found.' Returns the snippet."""
     logging.info("Retrieving snippet {!r}".format(name,))
     cursor = connection.cursor()
     command = "select * from snippets where keyword=%s"
     cursor.execute(command, (name,))
-    cursor.fetchone()
     connection.commit()
+    row = cursor.fetchone()
     logging.debug("Snippet retrieved successfully.")
-    return name
+    if not row:
+        # No snippet was found with that name.
+        return "404: Snippet Not Found"
+    return row[0]
 
 
 def main():
