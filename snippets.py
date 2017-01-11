fe18ca59ac5f640d1cad787490ee62dd07fc6b7b
Jon Thorpe <jonthrp@yahoo.com>
2017-01-11 15:45:59 -0800

    working on snippet catalogue

--- snippets.py
+++ snippets.py
@@ -14,32 +14,38 @@ def put(name, snippet):
     """Store a snippet with an associated name."""
     logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
     cursor = connection.cursor()
-    try:
-        command = "insert into snippets values (%s, %s)"
-        cursor.execute(command, (name, snippet))
-    except psycopg2.IntegrityError as e:
-        connection.rollback()
-        command = "update snippets set message=%s where keyword=%s"
-        cursor.execute(command, (snippet, name))
-    logging.debug("Snippet stored successfully.")
+    with connection, connection.cursor() as cursor:
+        if psycopg2.IntegrityError:
+            cursor.execute("update snippets set message=%s where keyword=%s", (snippet, name))
+        else:
+            cursor.execute("insert into snippets values (%s, %s)", (name, snippet))
     return name, snippet
+    logging.debug("Snippet stored successfully.")
+
 
 
 def get(name):
-    """Retrieve the snippet with a given name. If no snippet exists with that name,
+    """Retrieve the snippet with a given name. If there is no such snippet,
     return '404: Snippet Not Found.' Returns the snippet."""
     logging.info("Retrieving snippet {!r}".format(name,))
-    cursor = connection.cursor()
-    command = "select * from snippets where keyword=%s"
-    cursor.execute(command, (name,))
-    connection.commit()
-    row = cursor.fetchone()
-    logging.debug("Snippet retrieved successfully.")
+    with connection, connection.cursor() as cursor:
+        cursor.execute("select message from snippets where keyword=%s", (name,))
+        row = cursor.fetchone()
     if not row:
         # No snippet was found with that name.
         return "404: Snippet Not Found"
+    logging.debug("Snippet retrieved successfully.")
     return row[0]
 
+def catalogue():
+    """Allows the user to to query the keywords from the snippets table."""
+    logging.info("Retrieving keywords")
+    cursor = connection.cursor()
+    cursor.execute("select * from snippets order by keyword")
+    row = cursor.fetchall()
+    connection.commit()
+    logging.debug("Keywords retrieved successfully.")
+    return row[0:]
 
 def main():
     """Main function"""
@@ -54,10 +60,14 @@ def main():
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
 
-    # Subparder for the get command
+    # Subparser for the get command
     logging.debug("Constructing get subparser")
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="Name of the snippet")
+
+    # Subparser for the catalogue command
+    logging.debug("Constructing catalogue subparser")
+    catalogue_parser = subparsers.add_parser("catalogue", help="List keywords in snippers table")
 
     arguments = parser.parse_args()
     # Convert parsed arguments from Namespace to dictionary
@@ -70,6 +80,9 @@ def main():
     elif command == "get":
         snippet = get(**arguments)
         print("Retrieved snippet: {!r}".format(snippet))
+    elif command == "catalogue":
+        catalogue()
+        print("Retrieved catalogue")
 
 
 if __name__=="__main__":
